980a68d0cd4ff8be68eaec1a902fc16d06fd5fa1
emiliosnic <nicolaouemilios@gmail.com>
2017-06-07 18:11:16 +0300

    feat(http-hal): Adds store for HttpHal

--- test/lib/http-store.spec.js
+++ test/lib/http-store.spec.js
@@ -2,16 +2,16 @@ const tape = require('tape');
 const nock = require('nock');
 const tapeNock = require('tape-nock')
 
-const NotFoundError = require('stormer').errors.NotFoundError;
-const AlreadyExistsError = require('stormer').errors.AlreadyExistsError;
+const stormer = require('stormer');
+const NotFoundError = stormer.errors.NotFoundError;
+const AlreadyExistsError = stormer.errors.AlreadyExistsError;
 const HttpResponseError = require('../../lib/errors/HttpResponseError');
 const HttpStore = require('../../lib/http-store');
 
-
 const test = tapeNock(tape, { //options object to be passed to nock, not required
   defaultTestOptions: { // optionally provide default options to nockBack for each test
-    before: function () {},
-    after: function () {
+    before: function() {},
+    after: function() {
       nock.restore();
     }
   }
@@ -40,12 +40,12 @@ const model = {
 /**
  * Returns an HttpStore instance
  *
- * @function generateHttpStore
+ * @function generateStore
  * @param {Object} params
  * @return {HttpStore}
  *
  */
-const generateHttpStore = (alias, baseUrl) => {
+const generateStore = (alias, baseUrl) => {
   const store = new HttpStore({baseUrl});
   store.define('api_items', model);
   store.alias(alias, 'api_items');
@@ -56,8 +56,13 @@ const generateHttpStore = (alias, baseUrl) => {
  * Tests: HttpStore.constructor()
  */
 
-test('new HttpStore() should throw an error for', (t) => {
-  t.test('missing options', (assert) => {
+test('new HttpStore() should', (t) => {
+  t.test('be instance of Store', (assert) => {
+    assert.plan(1);
+    const store = new HttpStore({baseUrl: 'http://endpoint.mock.com/v1'});
+    assert.ok(store instanceof  stormer.Store);
+  });
+  t.test('throw an error for missing options', (assert) => {
     assert.plan(1);
     try {
       new HttpStore();
@@ -76,13 +81,12 @@ test('store._requestPromise(options) should reject with error on network failure
   // Setup request stub
   nock('http://endpoint.mock.com/v1').get('/api_items/b9d4621e-4abd-11e7-aa99-92ebcb67fe33').reply(500);
   // Setup store
-  const store = generateHttpStore('items', 'http://endpoint.mock.com/v1');
+  const store = generateStore('items', 'http://endpoint.mock.com/v1');
   // Assert rejection
   store._requestPromise({method: 'GET', url: '/items/b9d4621e-4abd-11e7-aa99-92ebcb67fe33'}).catch((err) => {
     assert.equals(err.message, 'Failed to fetch from endpoint');
   });
 });
-
 
 /**
  * Tests: HttpStore.get(model, pk)
@@ -93,7 +97,7 @@ test('store.get(model, pk) should resolve with an object', (assert) => {
   // Setup request stub
   nock('http://endpoint.mock.com/v1').get('/api_items/b9d4621e-4abd-11e7-aa99-92ebcb67fe33').reply(200, {id: 'b9d4621e-4abd-11e7-aa99-92ebcb67fe33'});
   // Setup store
-  const store = generateHttpStore('items', 'http://endpoint.mock.com/v1');
+  const store = generateStore('items', 'http://endpoint.mock.com/v1');
   // Assert correct `get`
   store.get('items', 'b9d4621e-4abd-11e7-aa99-92ebcb67fe33').then((actual) => {
     assert.deepEquals(actual, {id: 'b9d4621e-4abd-11e7-aa99-92ebcb67fe33'});
@@ -105,7 +109,7 @@ test('store.get(model, pk) should reject with a NotFoundError if model is not fo
   // Setup request stub
   nock('http://endpoint.mock.com/v1').get('/api_items/b9d4621e-4abd-11e7-aa99-92ebcb67fe33').reply(404, {})
   // Setup store
-  const store = generateHttpStore('items', 'http://endpoint.mock.com/v1');
+  const store = generateStore('items', 'http://endpoint.mock.com/v1');
   // Assert correct `get`
   store.get('items', 'b9d4621e-4abd-11e7-aa99-92ebcb67fe33').catch((err) => {
     assert.ok(err instanceof NotFoundError);
@@ -120,11 +124,11 @@ test('store.get(model, pk) should reject with a NotFoundError if model is not fo
 test('store.filter(model, query) should resolve with error on failure', (assert) => {
   assert.plan(2);
   // Setup request stub
-  nock('http://endpoint.mock.com/v1').get('/api_items?foo=bar').reply(400)
+  nock('http://endpoint.mock.com/v1').get('/api_items?name=Foo').reply(400)
   // Setup store
-  const store = generateHttpStore('items', 'http://endpoint.mock.com/v1');
+  const store = generateStore('items', 'http://endpoint.mock.com/v1');
   // Assert correct `filter`
-  store.filter('items', {foo: 'bar'}).catch((err) => {
+  store.filter('items', {name: 'Foo'}).catch((err) => {
     assert.ok(err instanceof HttpResponseError);
     assert.equals(err.message, 'Invalid filter response');
   });
@@ -136,14 +140,14 @@ test('store.filter(model, query) should resolve with correct data if results are
   const data = [
     {
       id: 'b9d4621e-4abd-11e7-aa99-92ebcb67fe33',
-      'foo': 'bar'
+      name: 'Foo'
     }
   ];
-  nock('http://endpoint.mock.com/v1').get('/api_items?foo=bar').reply(200, {data: data})
+  nock('http://endpoint.mock.com/v1').get('/api_items?name=Foo').reply(200, {data: data})
   // Setup store
-  const store = generateHttpStore('items', 'http://endpoint.mock.com/v1');
+  const store = generateStore('items', 'http://endpoint.mock.com/v1');
   // Assert correct `filter`
-  store.filter('items', {foo: 'bar'}).then((res) => {
+  store.filter('items', {name: 'Foo'}).then((res) => {
     assert.deepEquals(res, {data: data});
   });
 });
@@ -152,26 +156,26 @@ test('store.filter(model, query) should resolve with correct data if results are
  * Tests: HttpStore.delete(model, pk)
  */
 
-test('store.delete(model, pk) should resolve with success code', (assert) => {
+test('store.delete(model, query) should resolve with success code', (assert) => {
   assert.plan(1);
   // Setup request stub
   nock('http://endpoint.mock.com/v1').delete('/api_items/b9d4621e-4abd-11e7-aa99-92ebcb67fe33').reply(204)
   // Setup store
-  const store = generateHttpStore('items', 'http://endpoint.mock.com/v1');
+  const store = generateStore('items', 'http://endpoint.mock.com/v1');
   // Assert correct `delete`
-  store.delete('items', 'b9d4621e-4abd-11e7-aa99-92ebcb67fe33').then((res) => {
+  store.delete('items', {id: 'b9d4621e-4abd-11e7-aa99-92ebcb67fe33'}).then((res) => {
     assert.ok(res);
   });
 });
 
-test('store.delete(model, pk) should reject with error on failure', (assert) => {
+test('store.delete(model, query) should reject with error on failure', (assert) => {
   assert.plan(2);
   // Setup request stub
   nock('http://endpoint.mock.com/v1').delete('/api_items/b9d4621e-4abd-11e7-aa99-92ebcb67fe33').reply(400)
   // Setup store
-  const store = generateHttpStore('items', 'http://endpoint.mock.com/v1');
+  const store = generateStore('items', 'http://endpoint.mock.com/v1');
   // Assert correct `delete`
-  store.delete('items', 'b9d4621e-4abd-11e7-aa99-92ebcb67fe33').catch((err) => {
+  store.delete('items', {id: 'b9d4621e-4abd-11e7-aa99-92ebcb67fe33'}).catch((err) => {
     assert.ok(err instanceof HttpResponseError);
     assert.equals(err.message, 'Invalid "delete" response');
   });
@@ -183,11 +187,14 @@ test('store.delete(model, pk) should reject with error on failure', (assert) => 
 
 test('store.create(model, data) should reject with error on `create` operation failure', (assert) => {
   assert.plan(2);
-  const data = {id: 'b9d4621e-4abd-11e7-aa99-92ebcb67fe33', 'foo': 'bar'};
+  const data = {
+    id: 'b9d4621e-4abd-11e7-aa99-92ebcb67fe33',
+    name: 'Foo'
+  };
   // Setup request stub
   nock('http://endpoint.mock.com/v1').post('/api_items').reply(400);
   // Setup store
-  const store = generateHttpStore('items', 'http://endpoint.mock.com/v1');
+  const store = generateStore('items', 'http://endpoint.mock.com/v1');
   // Assert correct `create`
   store.create('items', data).catch((err) => {
     assert.ok(err instanceof HttpResponseError);
@@ -197,11 +204,14 @@ test('store.create(model, data) should reject with error on `create` operation f
 
 test('store.create(model, data) should reject with `AlreadyExistsError` error on `create` operation if model already exists', (assert) => {
   assert.plan(2);
-  const data = {id: 'b9d4621e-4abd-11e7-aa99-92ebcb67fe33', 'foo': 'bar'};
+  const data = {
+    id: 'b9d4621e-4abd-11e7-aa99-92ebcb67fe33',
+    'name': 'Foo'
+  };
   // Setup request stub
   nock('http://endpoint.mock.com/v1').post('/api_items').reply(409);
   // Setup store
-  const store = generateHttpStore('items', 'http://endpoint.mock.com/v1');
+  const store = generateStore('items', 'http://endpoint.mock.com/v1');
   // Assert correct `create`
   store.create('items', data).catch((err) => {
     assert.ok(err instanceof AlreadyExistsError);
@@ -211,11 +221,14 @@ test('store.create(model, data) should reject with `AlreadyExistsError` error on
 
 test('store.create(model, data) should resolve with correct data on `create` operation', (assert) => {
   assert.plan(1);
-  const data = {id: 'b9d4621e-4abd-11e7-aa99-92ebcb67fe33', 'foo': 'bar'};
+  const data = {
+    id: 'b9d4621e-4abd-11e7-aa99-92ebcb67fe33',
+    name: 'Foo'
+  };
   // Setup request stub
   nock('http://endpoint.mock.com/v1').post('/api_items').reply(201, data);
   // Setup store
-  const store = generateHttpStore('items', 'http://endpoint.mock.com/v1');
+  const store = generateStore('items', 'http://endpoint.mock.com/v1');
   // Assert correct `create`
   store.create('items', data).then((res) => {
     assert.deepEquals(res, data);
@@ -228,11 +241,14 @@ test('store.create(model, data) should resolve with correct data on `create` ope
 
 test('store.update(model, data) should reject with error on `update` operation failure', (assert) => {
   assert.plan(2);
-  const data = {id: 'b9d4621e-4abd-11e7-aa99-92ebcb67fe33', 'foo': 'bar'};
+  const data = {
+    id: 'b9d4621e-4abd-11e7-aa99-92ebcb67fe33',
+    name: 'Foo'
+  };
   // Setup request stub
   nock('http://endpoint.mock.com/v1').put('/api_items/b9d4621e-4abd-11e7-aa99-92ebcb67fe33').reply(400);
   // Setup store
-  const store = generateHttpStore('items', 'http://endpoint.mock.com/v1');
+  const store = generateStore('items', 'http://endpoint.mock.com/v1');
   // Assert correct `update`
   store.update('items', data).catch((err) => {
     assert.ok(err instanceof HttpResponseError);
@@ -242,11 +258,14 @@ test('store.update(model, data) should reject with error on `update` operation f
 
 test('store.update(model, data) should resolve with correct data on `update` operation', (assert) => {
   assert.plan(1);
-  const data = {id: 'b9d4621e-4abd-11e7-aa99-92ebcb67fe33', 'foo': 'bar'};
+  const data = {
+    id: 'b9d4621e-4abd-11e7-aa99-92ebcb67fe33',
+    name: 'Foo'
+  };
   // Setup request stub
   nock('http://endpoint.mock.com/v1').put('/api_items/b9d4621e-4abd-11e7-aa99-92ebcb67fe33').reply(200, data);
   // Setup store
-  const store = generateHttpStore('items', 'http://endpoint.mock.com/v1');
+  const store = generateStore('items', 'http://endpoint.mock.com/v1');
   // Assert correct `update`
   store.update('items', data).then((actual) => {
     assert.deepEquals(actual, data);
